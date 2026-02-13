5f0d37459ef1ae6115c2656631b90213efd0ef1b
josue <mendezjosue165@gmail.com>
2026-02-12 12:38:23 -1200

    clases heredadas, cliente y asiento

---
+++ src/main/java/Model/BoletoEstudiante.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author josue
+ */
+public class BoletoEstudiante extends Boleto {
+
+    public BoletoEstudiante(String boletoId) {
+        super(boletoId);
+    }
+
+    @Override
+    public double calcularPrecioFinal(double precioBase) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/Model/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author josue
+ */
+public class Cliente {
+    
+    private String idCliente;
+    
+    
+}
