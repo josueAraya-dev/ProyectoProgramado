fec2db3d2f31cf4d2b8367009bc6a0576a2f4cc2
josue <mendezjosue165@gmail.com>
2026-02-13 13:49:48 -1200

    desarrollo de clase cliente

--- src/main/java/Model/Clientes.java
+++ src/main/java/Model/Clientes.java
@@ -10,12 +10,28 @@ package Model;
  */
 public class Clientes {
     
+    private String nombre;
     private String idCliente;
 
-    public Clientes(String idCliente) {
+    public Clientes(String nombre, String idCliente) {
+        this.nombre = nombre;
         this.idCliente = idCliente;
     }
-    
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
     
 }
